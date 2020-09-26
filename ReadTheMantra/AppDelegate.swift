b9397db9f3760a0dd665bd15d3a0f356196d5863
mosariot <56510738+mosariot@users.noreply.github.com>
2020-09-26 10:32:41 +0300

    First launch refactoring

--- ReadTheMantra/AppDelegate.swift
+++ ReadTheMantra/AppDelegate.swift
@@ -30,27 +30,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
     //MARK: - Preload Data Stack
     
     func preloadData() {
-        removeData()
         let context = persistentContainer.viewContext
-        
         for (index, data) in InitialMantra.data.enumerated() {
             let mantra = Mantra(context: context)
             mantra.position = Int32(index)
-            mantra.isFavorite = false
             for (key, value) in data {
                 switch key {
                 case .title:
@@ -70,19 +62,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 }
             }
             saveContext()
-        }
-    }
-    
-    func removeData() {
-        let context = persistentContainer.viewContext
-        let request: NSFetchRequest<Mantra> = Mantra.fetchRequest()
-        do {
-            let mantraArray = try context.fetch(request)
-            for mantra in mantraArray {
-                context.delete(mantra)
-            }
-        } catch {
-            print("Error fetching data from context \(error)")
         }
     }
     
