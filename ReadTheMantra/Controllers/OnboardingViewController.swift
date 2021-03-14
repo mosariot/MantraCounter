94bd937ea91dd6a0f25c332c57d7bebdfd430b09
mosariot <56510738+mosariot@users.noreply.github.com>
2021-03-14 10:56:43 +0300

    Small onboarding text improvements. v1.0.1

--- ReadTheMantra/Controllers/OnboardingViewController.swift
+++ ReadTheMantra/Controllers/OnboardingViewController.swift
@@ -41,7 +41,7 @@ final class OnboardingViewController: UIViewController {
                                     Recitation of mantras - is a sacrament.
                                     Approach this issue with all your awareness.
                                     In order for the practice of reciting the mantra to be correct, one must receive the transmission of the mantra from the teacher. Transmission is essential to maintain the strength of the original source of the mantra. It is not enough just to read it in a book or on the Internet.
-                                    For this reason, application doesn't include the mantra texts themselves (except Vajrasattva). They must be given to you by your spiritual mentor.
+                                    For this reason, at start application doesn't include the mantra texts themselves (except Vajrasattva). They must be given to you by your spiritual mentor and can be added manually later.
                                     We wish you deep awarenesses and spiritual growth!
                                     """, comment: "Onboarding Alert Message")
         
